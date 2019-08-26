f5ccaa71fcdd88ee228da41e26ff059626b868a0
4fr0c0d3 <ngigemoffat@gmail.com>
2019-08-26 21:36:21 +0300

    chore(refactor): refector UI components

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+
 import './App.css';
+
+import avatar from '../src/assets/images/avatar.jpg';
+import logout from '../src/assets/images/logout.svg';
+import min from '../src/assets/images/min.svg';
+import max from '../src/assets/images/max.svg';
+import next from '../src/assets/images/next.svg';
+import previous from '../src/assets/images/previous.svg';
+import numbers from '../src/assets/images/numbers.svg';
+
 import Row from './Row';
-import phoneNumberGenerator from './utils/generateNumbers';
+import Nav from '../src/components/nav';
+import Dashboard from '../src/components/dashboard';
+
+
+
+import phoneNumberGenerator from './utils/randomNumberGenerator';
 
 export default class App extends Component {
   state = {
@@ -10,6 +25,29 @@ export default class App extends Component {
     error: null,
     asc: false
   }
+
+  cards = [
+    {
+        icon: numbers,
+        header: 'Total Numbers',
+        text: '10, 000'
+    },
+    {
+        icon: min,
+        header: 'Min Number',
+        text: '0156789876'
+    },
+    {
+        icon: max,
+        header: 'Max Number',
+        text: '0987654567'
+    }
+  ]
+
+  dropdownOptions = [
+    'Ascending',
+    'Descending'
+  ]
 
   componentDidMount() {
     const numbers = JSON.parse(localStorage.getItem('numbers'));
@@ -47,45 +85,53 @@ export default class App extends Component {
     }
   }
 
+renderSidePane = () => (
+      <div className="side-pane">
+      <span>
+          <img 
+              src={''}
+              className="side-pane-avatar"
+              alt={''}
+              >
+          </img>
+      </span>
+      <span className="logo">
+      𝔫𝔲𝔪𝔟𝔢𝔯𝔰
+      </span>
+      <span>
+          <img 
+              src={''}
+              className="side-pane-logout"
+              alt={''}
+              >
+          </img>
+      </span>
+    </div>
+  )
+
+  renderSideNavIcon = () => (
+      <span className="side-pane-icons">
+        <img 
+            className={`side-pane-${''}`}
+            src={''} 
+            alt={''}               
+            >
+        </img>
+      </span>
+  )
+
   render() {
     const { numbers, error, amount } = this.state;
     const disable = amount > 10000 || amount < 1;
 
     return (
-      <div className="page">
-        <div className="card">
-          <div>
-            <h1>PhoneX</h1>
-            <form>
-              <div className="form-input">
-                <input
-                  name="phone"
-                  type="number"
-                  placeholder="Amount to be generated"
-                  onChange={this.handleChange}
-                  className={
-                   /* istanbul ignore next */
-                    error ? 'error-input' : ''}
-                />
-                {
-                  /* istanbul ignore next */
-                  disable && amount !== null && <div className="error">Amount must be between 1 - 10000</div>
-                }
-              </div>
-              <button type="submit" onClick={this.generateNumbers} disabled={disable}>Generate</button>
-            </form>
-          </div>
-
-          <p>Random Phone Number Generator App</p>
-        </div>
-        <div className="card">
-          <Row className data={{ index: "Index", phone: 'Phone Numbers' }} onClick={this.handleSort} />
-          {numbers && numbers.map((phone, idx) => {
-            /* istanbul ignore next */
-            return <Row key={idx} data={{ index: idx + 1, phone }} />
-          })}
-          {numbers.length < 1 && <div className="centered">You haven't generated any phone numbers yet</div>}
-        </div>
+      <div className="App-header">
+        <Nav />
+        <Dashboard cards={this.cards} options={this.dropdownOptions}/>
+      { this.renderSidePane() }
+     
+        
+        
       </div>
     );
   }
