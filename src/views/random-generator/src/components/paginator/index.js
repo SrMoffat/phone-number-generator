f20ffcbabcada2c2cc7e705b9b422c8ee16fd275
4fr0c0d3 <ngigemoffat@gmail.com>
2019-08-26 11:19:00 +0300

    feat(paginator): add paginator component
    - add paginator component
    - add styles for paginator

--- src/views/random-generator/src/components/paginator/index.js
+++ src/views/random-generator/src/components/paginator/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 
+import previous from '../../assets/images/previous.svg';
+import next from '../../assets/images/next.svg';
+
+const onClickNext = (e) => {
+    console.log(e.target);
+}
+
+const onClickPrev = (e) => {
+    console.log(e.target);
+}
+
 const Paginator = () => {
     return (
-        <div className="paginator">Paginator</div>
+        <div className="paginator">
+            <span className="previous-page">
+                <img
+                    src={previous}
+                    alt={previous}
+                    onClick={onClickPrev}        
+                    >
+                </img>  
+            </span>
+            <span className="page-count">
+                <span>Page </span>
+                <span className="current-page">2 </span>
+                <span> of</span>
+                <span className="total-pages"> 20</span>
+            </span>
+            <span className="previous-page">
+                <img 
+                    src={next}
+                    alt={next}
+                    onClick={onClickNext}        
+                    >
+                </img>  
+            </span>        
+        </div>
     )
 }
 
